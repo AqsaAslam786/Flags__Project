929b4021fc7b65d1fdd543ec34fe05c6f90b1a19
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-09-20 00:29:21 +0500

    Download packages

---
+++ my_project/src/App.jsx
@@ -0,0 +1,12 @@
+import Bangladash_Flag from "./Components/Bangladash_Flag"
+
+function App() {
+
+  return (
+    <>
+    <Bangladash_Flag />
+    </>
+  )
+}
+
+export default App
---
+++ my_project/src/Components/Bangladash_Flag.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Bangladash_Flag() {
+  return (
+    <div className='bg-green-600 h-70 w-120 flex justify-center items-center'>
+        <div className='bg-red-700 h-35 w-35 rounded-full -ml-30'></div>
+    </div>
+  )
+}
